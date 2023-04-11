926dde568b0f8681d59c761135977970db2a222e
amand <lafaetehmiranda@gmail.com>
2023-04-11 11:45:12 -0300

    comit teste

--- src/classeExecutavel/PrimeiraCalsse.java
+++ src/classeExecutavel/PrimeiraCalsse.java
@@ -4,5 +4,9 @@ public class PrimeiraCalsse {
 
     public static void main(String[] args) {
 
+        String oi = "Hello world!";
+        String oi1eth = "teste";
+        System.out.println(oi + oi1eth);
+
     }
 }
